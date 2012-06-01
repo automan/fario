93e937abc8d5d0b0ab551208afbbc410c845a11f
Taichan <taichan@taobao.com>
2012-06-01 15:38:58 +0800

    use post

--- src/main/java/com/taobao/fario/server/servlet/RegisterServlet.java
+++ src/main/java/com/taobao/fario/server/servlet/RegisterServlet.java
@@ -4,6 +4,7 @@
 package com.taobao.fario.server.servlet;
 
 import java.io.IOException;
+import java.net.URLDecoder;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -32,6 +33,10 @@ public class RegisterServlet extends HttpServlet {
 		String accuracy = req.getParameter("acc");
 		String username = req.getParameter("uid");
 
+		if (username != null) {
+			username = URLDecoder.decode(username, "UTF-8");
+		}
+
 		LocationInfo locationInfo = new LocationInfo(time, latitude, longitude,
 				altitude, accuracy, username);
 
@@ -46,4 +51,34 @@ public class RegisterServlet extends HttpServlet {
 
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest
+	 * , javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)	
+			throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+		String time = req.getParameter("time");
+		String latitude = req.getParameter("la");
+		String longitude = req.getParameter("lo");
+		String altitude = req.getParameter("al");
+		String accuracy = req.getParameter("acc");
+		String username = req.getParameter("uid");
+
+		LocationInfo locationInfo = new LocationInfo(time, latitude, longitude,
+				altitude, accuracy, username);
+
+		LocationInfo last = UserHistory.getInstance().last(locationInfo);
+		UserHistory.getInstance().add(locationInfo);
+
+		if (last != null) {
+			resp.getWriter().write(last.toString());
+		} else {
+			resp.getWriter().write("first time visit!");
+		}
+	}
 }
